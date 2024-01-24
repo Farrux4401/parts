1e8c13d361e073483df54d2f06b4977b2b1eb8dd
Farrux4401 <leganesalaves@gmail.com>
2024-01-24 21:47:03 +0500

    mobile

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ glide01.mount();
 
 var glide02 = new Glide(".glide-02", {
   type: "carousel",
-  focusAt: "center",
+  // focusAt: "right",
   perView: 3,
   autoplay: 3000,
   animationDuration: 700,
@@ -24,10 +24,10 @@ var glide02 = new Glide(".glide-02", {
   },
   breakpoints: {
     1024: {
-      perView: 2,
+      perView: 1.5,
     },
-    640: {
-      perView: 1,
+    768: {
+      perView: 1.5,
     },
   },
 });
@@ -36,7 +36,6 @@ glide02.mount();
 
 var glide03 = new Glide(".glide-03", {
   type: "carousel",
-  focusAt: "center",
   perView: 7,
   autoplay: 3000,
   animationDuration: 700,
@@ -46,12 +45,68 @@ var glide03 = new Glide(".glide-03", {
   },
   breakpoints: {
     1024: {
-      perView: 2,
+      perView: 5,
     },
     640: {
-      perView: 1,
+      perView: 2,
     },
   },
 });
 
 glide03.mount();
+
+var glide04 = new Glide(".glide-04", {
+  type: "carousel",
+  // focusAt: "center",
+  perView: 2,
+  autoplay: false,
+  animationDuration: 700,
+  gap: 20,
+  classes: {
+    activeNav: "[&>*]:bg-slate-700",
+  },
+  breakpoints: {
+    768: {
+      perView: 1,
+    },
+  },
+});
+
+glide04.mount();
+
+var glide05 = new Glide(".glide-05", {
+  type: "carousel",
+  perView: 1.5,
+  autoplay: false,
+  animationDuration: 700,
+  gap: 20,
+  classes: {
+    activeNav: "[&>*]:bg-slate-700",
+  },
+  breakpoints: {
+    768: {
+      perView: 1.3,
+    },
+  },
+});
+
+glide05.mount();
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdownBtn = document.getElementById("dropdown-btn");
+  const dropdownContent = document.getElementById("dropdown-content");
+
+  dropdownBtn.addEventListener("click", function () {
+    dropdownContent.classList.toggle("hidden");
+  });
+
+  // Close the dropdown when clicking outside of it
+  document.addEventListener("click", function (event) {
+    if (
+      !dropdownBtn.contains(event.target) &&
+      !dropdownContent.contains(event.target)
+    ) {
+      dropdownContent.classList.add("hidden");
+    }
+  });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "hero-pattern":
           "linear-gradient(90deg,#444,#838383 3%,#bebebe 5%,#fff 12%,#fff 20%,#a8a8a8 30%,#bebebe 32%,#fff 38%,#9c9c9c 46%,#404040 56%,#626262 60%,#b8b8b8 68%,#ececea 71%,#a1a1a1 80%,#fff 90%,#a1a1a1 94%,#3e3e3e)",
       },
+      screens: {
+        tablet: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        laptop: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: "1280px",
+        // => @media (min-width: 1280px) { ... }
+      },
     },
   },
   plugins: [],
